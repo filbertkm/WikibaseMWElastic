a0d370203e5914f636d1e077c67c5dcff782c33c
aude <aude.wiki@gmail.com>
2015-11-13 15:35:31 +0100

    Add search query maintenance script
    useful for debugging...

---
+++ maintenance/Queryer.php
@@ -0,0 +1,41 @@
+<?php
+
+use CirrusSearch\Connection;
+use ConfigFactory;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once ( "$IP/maintenance/Maintenance.php" );
+
+class Queryer extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Query Wikibase";
+	}
+
+	public function execute() {
+		$path = 'wikidatawiki_content_first/_search';
+		$data = array();
+
+		$config = ConfigFactory::getDefaultInstance()->makeConfig( 'CirrusSearch' );
+		$conn = Connection::getPool( $config );
+
+		$client = $conn->getClient();
+
+		$result = $client->request(
+			$path,
+			Elastica\Request::GET,
+			$data,
+			array()
+		);
+
+		echo $result;
+	}
+}
+
+$maintClass = "Queryer";
+require_once RUN_MAINTENANCE_IF_MAIN;
