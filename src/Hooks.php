7e14315edef3cfb125e80a8f81115934047daf70
aude <aude.wiki@gmail.com>
2015-11-13 15:35:55 +0100

    Add CirrusSearchExtractSpecialSyntax hook handling

--- src/Hooks.php
+++ src/Hooks.php
@@ -10,6 +10,7 @@ use ParserOutput;
 use Title;
 use Wikibase\Elastic\Document\DocumentTermsBuilder;
 use Wikibase\Elastic\Mapping\TermMappingBuilder;
+use Wikibase\EntityContent;
 use Wikibase\Lib\WikibaseContentLanguages;
 
 /**
@@ -64,6 +65,43 @@ class Hooks {
 		}
 
 		return true;
+	}
+
+	public static function onCirrusSearchExtractSpecialSyntax(
+		$key,
+		$searchText,
+		&$filterDestination,
+		&$searchContainedSyntax,
+		&$match
+	) {
+		$languageCodes = array( 'de', 'en', 'es' );
+		$keys = array_map( function( $key ) {
+				return "label-$key";
+			}, $languageCodes
+		);
+
+		preg_match( '/label-([\w]+)/', $key, $matches );
+
+		if ( isset( $matches[1] ) ) {
+			$languageCode = $matches[1];
+			$searchContainedSyntax = true;
+
+			$nested = new \Elastica\Filter\Nested();
+			$nested->setPath( 'labels' );
+			$nested->setFilter(
+				new \Elastica\Filter\Query(
+					new \Elastica\Query\Match( $languageCode, $searchText )
+				)
+			);
+
+			$filterDestination[] = $nested;
+		}
+
+		return false;
+	}
+
+	public static function onWikibaseTextForSearchIndex( EntityContent $content, &$text ) {
+		$text = '';
 	}
 
 }
