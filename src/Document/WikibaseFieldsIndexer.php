bfdf2deeb915a448c1c38580bdf5b9246f632e1c
aude <aude.wiki@gmail.com>
2015-11-17 12:47:48 +0100

    Make TermFieldsBuilder more generic, with other type of fields
    add fields for description, entity type and sitelink count

---
+++ src/Document/WikibaseFieldsIndexer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Wikibase\Elastic\Document;
+
+use Content;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\EntityContent;
+
+class WikibaseFieldsIndexer {
+
+	/**
+	 * @param Content $content
+	 *
+	 * @return array
+	 */
+	public function build( Content $content ) {
+		if ( !$content instanceof EntityContent || $content->isRedirect() === true ) {
+			return array();
+		}
+
+		$entity = $content->getEntity();
+		$terms = $entity->getFingerprint();
+
+		$fields = array(
+			'labels' => $this->indexLabels( $terms ),
+			'descriptions' => $this->indexDescriptions( $terms ),
+			'entity_type' => $entity->getType(),
+			'sitelink_count' => 0
+		);
+
+		if ( $entity instanceof Item ) {
+			$fields['sitelink_count'] = $entity->getSiteLinkList()->count();
+		}
+
+		return $fields;
+	}
+
+	/**
+	 * @param Fingerprint $terms
+	 *
+	 * @return array
+	 */
+	private function indexLabels( Fingerprint $terms ) {
+		return $this->buildTermsData( $terms->getLabels() );
+	}
+
+	/**
+	 * @param Fingerprint $terms
+	 *
+	 * @return array
+	 */
+	private function indexDescriptions( Fingerprint $terms ) {
+		return $this->buildTermsData( $terms->getDescriptions() );
+	}
+
+	/**
+	 * @param TermList $terms
+	 *
+	 * @return array
+	 */
+	private function buildTermsData( $terms ) {
+		$termsArray = array();
+
+		foreach ( $terms->toTextArray() as $languageCode => $term ) {
+			$termsArray[$languageCode] = $term;
+		}
+
+		return $termsArray;
+	}
+
+}
